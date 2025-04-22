d28ed98526fc79183726f54d4e1e9ba19211dc7c
Tushar Gaurav <tushar.gaurav416@gmail.com>
2025-04-22 14:59:32 +0530

    fix: hero

--- styles/index.js
+++ styles/index.js
@@ -18,7 +18,7 @@ const styles = {
   heroHeading:
       'font-bold lg:text-[114px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
   heroOText:
-      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[90px] sm:h-[48px] h-[38px] md:border-[18px] sm:border-[8px] rounded-[50px] border-white sm:mx-2 mx-[6px]',
+      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[90px] sm:h-[48px] h-[38px] md:border-[18px] sm:border-[8px] border-[6px] rounded-[50px] border-white sm:mx-2 mx-[6px]',
 };
 
 export default styles;
