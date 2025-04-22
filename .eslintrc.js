d568e72896ae187fb3a44515255172fc533578a7
Tushar Gaurav <tushar.gaurav416@gmail.com>
2025-04-22 14:23:05 +0530

    fix: general improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
+    'no-trailing-spaces': 0,
     'react/react-in-jsx-scope': 'off',
     'react/jsx-props-no-spreading': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
